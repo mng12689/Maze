c601a523e6d756a1bc6dbaba60a6d926ef9dca7b
mng12689 <mng12689@gmail.com>
2012-09-05 13:04:32 -0700

    add check for game over

--- Maze/MazeViewController.m
+++ Maze/MazeViewController.m
@@ -34,7 +34,7 @@
         self.mazeView.delegate = self;
         self.view = self.mazeView;
 
-        self.session = [[GKSession alloc]initWithSessionID:@"ManeeshMaze" displayName:@"Maneesh" sessionMode:GKSessionModePeer];
+        self.session = [[GKSession alloc]initWithSessionID:@"ManeeshMaze" displayName:@"Mike" sessionMode:GKSessionModePeer];
         [self.session setDataReceiveHandler:self withContext:nil];
         self.session.delegate = self;
         self.session.available = YES;
@@ -119,7 +119,9 @@
     [self.mazeView placeBall];
     self.ballLocation = self.mazeView.ballLocation;
     [self.motionManager startDeviceMotionUpdatesToQueue:[NSOperationQueue currentQueue]  withHandler:^(CMDeviceMotion *motion, NSError *error) {
-        [self updateBallPosition];
+        if (!self.gameOver) {
+            [self updateBallPosition];
+        }
     }];
 }
 
